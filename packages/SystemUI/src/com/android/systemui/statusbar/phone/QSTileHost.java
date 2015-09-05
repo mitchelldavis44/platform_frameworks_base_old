6d2e2115cc4ab76caeaebec3c718ffdcd535e795
Mitch Davis <mitchelldavis44@gmail.com>
2015-09-05 16:55:04 -0700

    Revert "SystemUI: Create LteTile"
    This reverts commit a620cf2c428da48473387398f9d4ebaab25b8135.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,7 +43,6 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
-import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
@@ -290,9 +289,7 @@ public class QSTileHost implements QSTile.Host {
             case QSConstants.TILE_COMPASS:
                 return new CompassTile(this);
             case QSConstants.TILE_NFC:
-                return new NfcTile(this);   
-            case QSConstants.TILE_LTE:
-                return new LteTile(this);
+                return new NfcTile(this);
 	    case QSConstants.TILE_SCREEN_TIMEOUT:
                 return new ScreenTimeoutTile(this);
             default:
